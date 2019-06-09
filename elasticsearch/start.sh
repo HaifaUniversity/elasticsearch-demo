bfb9515ce8844dffd7da839562a3534d86bece03
Phillip Tribble <phillip.tribble@gmail.com>
2019-06-08 20:18:16 -0600

    v7.1.1

--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -4,12 +4,14 @@ FILE='/configured.txt'
 if [ ! -f "$FILE" ]
 then
   echo 'network.host: ' `echo ${HOSTNAME}` >> /opt/elasticsearch*/config/elasticsearch.yml
-  echo 'node.name: ' `echo ${HOSTNAME}` >> /opt/elasticsearch*/config/elasticsearch.yml
+  echo 'node.name: ' `echo ${name}` >> /opt/elasticsearch*/config/elasticsearch.yml
   echo 'discovery.zen.ping.unicast.hosts:' `echo $nodes` >> /opt/elasticsearch*/config/elasticsearch.yml
+  echo 'cluster.initial_master_nodes: ["elasticsearch-master", "elasticsearch-slave1", "elasticsearch-slave2"]' >> /opt/elasticsearch*/config/elasticsearch.yml
   sed -i 's/2g/200m/g' /opt/elasticsearch*/config/jvm.options
 
   if [ "$mode" == "master" ]; then
   echo 'node.master: true' >> /opt/elasticsearch*/config/elasticsearch.yml
+  echo 'node.data: true' >> /opt/elasticsearch*/config/elasticsearch.yml
   fi
 
   if [ "$mode" == "slave" ]; then
